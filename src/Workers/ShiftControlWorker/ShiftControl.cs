8fe162929c3675617cdec9eb056fb4bf7c9a0604
Serdar Bilek <serdarbilek@outlook.com>
2023-08-28 19:50:14 +0300

    add serviceProvider to fix local instance problem

--- src/Workers/ShiftControlWorker/ShiftControl.cs
+++ src/Workers/ShiftControlWorker/ShiftControl.cs
@@ -1,33 +1,43 @@
 using Business.IServices;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ShiftControlWorker
 {
     public class ShiftControl : BackgroundService
     {
         private readonly ILogger<ShiftControl> _logger;
-        private readonly ITeamService teamService;
+        private readonly IServiceProvider serviceProvider;
 
-        public ShiftControl(ILogger<ShiftControl> logger, ITeamService _teamService)
+        public ShiftControl(ILogger<ShiftControl> logger, IServiceProvider _serviceProvider)
         {
             _logger = logger;
-            teamService = _teamService;
+            serviceProvider = _serviceProvider;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+
                 _logger.LogInformation("Shift Control Worker running at: {time}", DateTimeOffset.Now);
                 var haveTeamActiveChats = true;
-                while (haveTeamActiveChats)
+                using (IServiceScope scope = serviceProvider.CreateScope())
                 {
-                    haveTeamActiveChats = await teamService.CloseTeamThatNotActiveIfDoNotHaveActiveChats();
-                    if (haveTeamActiveChats)
+                    ITeamService teamService =
+                        scope.ServiceProvider.GetRequiredService<ITeamService>();
+
+                    while (haveTeamActiveChats)
                     {
-                        await teamService.PassiveTheStatusOfTheTeamWaitingForActiveChats();
+                        haveTeamActiveChats = await teamService.CloseTeamThatNotActiveIfDoNotHaveActiveChats();
+                        if (haveTeamActiveChats)
+                        {
+                            await teamService.PassiveTheStatusOfTheTeamWaitingForActiveChats();
+                        }
+                        await Task.Delay(TimeSpan.FromSeconds(8));
                     }
-                    await Task.Delay(TimeSpan.FromSeconds(8));
                 }
+            
+             
 
                 await Task.Delay(TimeSpan.FromHours(8), stoppingToken);
             }
